4bb9ecc2283e02517ebfea34f1e54d37c3c64df1
dilshod-14 <urolovdilshod24@gmail.com>
2025-02-26 16:53:09 +0900

    fix: removed unnecessary code

--- train.js
+++ train.js
@@ -11,15 +11,7 @@
 // console.log(ismlar("e", "elmurod"));
 // console.log(ismlar("o", "Shog,zod"));
 
-// function countDigits(str) {  // Funktsiyani to'g'ri e'lon qilish
-//     const result = str.split('').filter((ele) => {
-//       return ele === '7';  // '7' ni ajratib olish
-//     });
-//     return result;
-//   }
 
-//   const newcountDigits = countDigits("ad2a54y79wet0sfgb9");
-//   console.log(newcountDigits);  // ['7', '7']
 
 const liste = ["ad2a54y79wet0sfgb9"];
 const newListe = liste[0].split('').filter((ele) => {
