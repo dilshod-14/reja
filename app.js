53a5b30102753bb61468ce5357fb087d4bb85890
dilshod-14 <urolovdilshod24@gmail.com>
2025-03-03 03:03:22 +0900

    feat: FrontEnd javascripni qurish

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log("web serverni boshlash");
 const express = require("express");
-const res = require("express/lib/response");
+
 const app = express();
 
 //MongoDB chaqirish
@@ -21,12 +21,8 @@ app.post("/create-item", (req, res) => {
   console.log(req.body);
   const new_reja = req.body.reja;
   db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.end("something Went wrong ");
-    } else {
-      res.end("seccessfully addeed");
-    }
+    console.log(data.ops);
+    res.json(data.ops[0]);
   });
 });
 
@@ -38,12 +34,10 @@ app.get("/", function (req, res) {
       if (err) {
         console.log(err);
         res.end("something Went wrong");
-      } else { 
-      
+      } else {
         res.render("reja", { items: data });
       }
-    }); 
+    });
 });
 
 module.exports = app;
-    
