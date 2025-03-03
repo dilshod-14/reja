04e4f2acd69ed26427abc1f89bbc7772197029c6
dilshod-14 <urolovdilshod24@gmail.com>
2025-03-04 02:27:57 +0900

    BBR:tack c ni yechish

--- train.js
+++ train.js
@@ -1,21 +1,56 @@
-// function ismlar(e, dostlar) {
-//     let count = 0;
-//     for(let salom of dostlar) {
-//         if(salom === e) {
-//             count++;
-//         }
-//     }
-// return count;
-// }
+const moment = require("moment");
+class Shop {
+  constructor(non, lagmon, suv) {
+    this.non = non;
+    this.lagmon = lagmon;
+    this.suv = suv;
+  }
 
-// console.log(ismlar("e", "elmurod"));
-// console.log(ismlar("o", "Shog,zod"));
+  hisobat() {
+   
+    const time = moment().format("HH:mm:ss");
+    console.log(
+      `${time}Hozirgi holat: ${this.non}ta non, ${this.lagmon}ta lagmon, ${this.suv}ta suv bor.`
+    );
+  
+  }
+  qabul(mahsulot, miqdor) {
+    const time = moment().format("HH:mm:ss");
 
+   
+    if (mahsulot === "non") this.non += miqdor;
+    else if (mahsulot === "lagmon") this.lagmon += miqdor;
+    else if (mahsulot === "suv") this.suv += miqdor;
+    else {"Notogri mahsulot nomi!"};
+console.log( `${time} da ${miqdor} ta ${mahsulot} qabul qilindi.`);
+  }
 
+  sotish(mahsulot, miqdor) {
+    const time = moment().format("HH:mm:ss");
+    if (mahsulot == "non" && this.non >= miqdor) this.non -= miqdor;
+    else if (mahsulot == "lagmon" && this.lagmon >= miqdor)
+      this.lagmon -= miqdor;
+    else if (mahsulot == "suv" && this.suv >= miqdor) this.suv -= miqdor;
+    else return "Yetarlicha mahsulot yoq yoki notogri mahsulot nomi!";
 
-const liste = ["ad2a54y79wet0sfgb9"];
-const newListe = liste[0].split('').filter((ele) => {
-  return ele === "7";
-});
+    console.log( `${time} da ${miqdor} ta ${mahsulot} sotildi.`);
+  }
+}
 
-console.log("newListe", newListe);
+const myShop = new Shop(10, 10, 10);
+setTimeout(() => {
+  myShop.hisobat();
+}, 1000);
+setTimeout(() => {
+myShop.qabul("non",10 );  
+}, 2000);
+setTimeout(() => {
+ myShop.sotish("lagmon", 1);
+}, 3000);
+setTimeout(() => {
+ myShop.sotish("suv", 1);
+}, 4000);
+
+setTimeout(() => {
+  myShop.hisobat();
+}, 5000);
