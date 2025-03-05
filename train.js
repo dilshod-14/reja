170d27aa69fb05e39e0e86519e013ee9816ad1cb
dilshod-14 <urolovdilshod24@gmail.com>
2025-03-05 21:44:23 +0900

    BBR: Task D yechish

--- train.js
+++ train.js
@@ -1,56 +1,67 @@
-const moment = require("moment");
-class Shop {
-  constructor(non, lagmon, suv) {
-    this.non = non;
-    this.lagmon = lagmon;
-    this.suv = suv;
-  }
+// const moment = require("moment");
+// class Shop {
+//   constructor(non, lagmon, suv) {
+//     this.non = non;
+//     this.lagmon = lagmon;
+//     this.suv = suv;
+//   }
 
-  hisobat() {
-   
-    const time = moment().format("HH:mm:ss");
-    console.log(
-      `${time}Hozirgi holat: ${this.non}ta non, ${this.lagmon}ta lagmon, ${this.suv}ta suv bor.`
-    );
-  
-  }
-  qabul(mahsulot, miqdor) {
-    const time = moment().format("HH:mm:ss");
+//   hisobat() {
 
-   
-    if (mahsulot === "non") this.non += miqdor;
-    else if (mahsulot === "lagmon") this.lagmon += miqdor;
-    else if (mahsulot === "suv") this.suv += miqdor;
-    else {"Notogri mahsulot nomi!"};
-console.log( `${time} da ${miqdor} ta ${mahsulot} qabul qilindi.`);
-  }
+//     const time = moment().format("HH:mm:ss");
+//     console.log(
+//       `${time}Hozirgi holat: ${this.non}ta non, ${this.lagmon}ta lagmon, ${this.suv}ta suv bor.`
+//     );
 
-  sotish(mahsulot, miqdor) {
-    const time = moment().format("HH:mm:ss");
-    if (mahsulot == "non" && this.non >= miqdor) this.non -= miqdor;
-    else if (mahsulot == "lagmon" && this.lagmon >= miqdor)
-      this.lagmon -= miqdor;
-    else if (mahsulot == "suv" && this.suv >= miqdor) this.suv -= miqdor;
-    else return "Yetarlicha mahsulot yoq yoki notogri mahsulot nomi!";
+//   }
+//   qabul(mahsulot, miqdor) {
+//     const time = moment().format("HH:mm:ss");
 
-    console.log( `${time} da ${miqdor} ta ${mahsulot} sotildi.`);
-  }
+//     if (mahsulot === "non") this.non += miqdor;
+//     else if (mahsulot === "lagmon") this.lagmon += miqdor;
+//     else if (mahsulot === "suv") this.suv += miqdor;
+//     else {"Notogri mahsulot nomi!"};
+// console.log( `${time} da ${miqdor} ta ${mahsulot} qabul qilindi.`);
+//   }
+
+//   sotish(mahsulot, miqdor) {
+//     const time = moment().format("HH:mm:ss");
+//     if (mahsulot == "non" && this.non >= miqdor) this.non -= miqdor;
+//     else if (mahsulot == "lagmon" && this.lagmon >= miqdor)
+//       this.lagmon -= miqdor;
+//     else if (mahsulot == "suv" && this.suv >= miqdor) this.suv -= miqdor;
+//     else return "Yetarlicha mahsulot yoq yoki notogri mahsulot nomi!";
+
+//     console.log( `${time} da ${miqdor} ta ${mahsulot} sotildi.`);
+//   }
+// }
+
+// const myShop = new Shop(10, 10, 10);
+// setTimeout(() => {
+//   myShop.hisobat();
+// }, 1000);
+// setTimeout(() => {
+// myShop.qabul("non",10 );
+// }, 2000);
+// setTimeout(() => {
+//  myShop.sotish("lagmon", 1);
+// }, 3000);
+// setTimeout(() => {
+//  myShop.sotish("suv", 1);
+// }, 4000);
+
+// setTimeout(() => {
+//   myShop.hisobat();
+// }, 5000);
+
+function list(a, b) {
+  if (a.length !== b.length) return false;
+
+  let list1 = a.split("").sort().join("");
+  let list2 = b.split("").sort().join("");
+  return list1 === list2
+  // console.log(list1);
+  // console.log(list2);
 }
 
-const myShop = new Shop(10, 10, 10);
-setTimeout(() => {
-  myShop.hisobat();
-}, 1000);
-setTimeout(() => {
-myShop.qabul("non",10 );  
-}, 2000);
-setTimeout(() => {
- myShop.sotish("lagmon", 1);
-}, 3000);
-setTimeout(() => {
- myShop.sotish("suv", 1);
-}, 4000);
-
-setTimeout(() => {
-  myShop.hisobat();
-}, 5000);
+console.log(list("maktab", "bkmata"))
